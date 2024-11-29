7939783e986e93e9065a0ac14edc5815d2831a4c
Ubspy <jack-m@live.com>
2024-11-28 18:11:41 -0600

    Moved init.lua to the right spot

--- nvim/lua/config/init.lua
+++ nvim/lua/config/init.lua
@@ -1,7 +1,10 @@
+-- This one has to go first
+require("config.lazy")
+
+-- Rest of the configs
 require("config.colors")
 require("config.comments")
 require("config.git")
-require("config.lazy")
 require("config.lsp")
 require("config.lualine")
 require("config.remap")
