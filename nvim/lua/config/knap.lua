c9b488402c57920b9515fbc4e3407c3e2e240489
Ubspy <jack-m@live.com>
2024-12-06 00:12:08 -0600

    Made zathura reverse indexing work

--- nvim/lua/config/knap.lua
+++ nvim/lua/config/knap.lua
@@ -3,8 +3,7 @@ local gknapsettings = {
     textopdf = "pdflatex -synctex=1 -halt-on-error -interaction=batchmode %docroot%",
     textopdfviewerlaunch = "zathura %outputfile%",
     textopdfviewerrefresh = "kill -HUP %pid%",
-    -- TODO: I can't get control + click to work on zathura -> nvim jump
-    textopdfviewerlaunch = "zathura -x 'nvim --headless -es --cmd \"lua require(\"knaphelper\").relayjump(\"%servername%\", \"%srcfile%\", %line%, 0)\"' %outputfile%",
+    textopdfviewerlaunch = "zathura --synctex-editor-command='nvim --headless -e -c \"lua require('\"'\"'knaphelper'\"'\"').relayjump('\"'\"'%servername%'\"'\"', '\"'\"'%{input}'\"'\"', %{line}, 0)\"' %outputfile%",
     textopdfviewerrefresh = "none",
     textopdfforwardjump = "zathura --synctex-forward=%line%:%column%:%srcfile% %outputfile%"
 }
